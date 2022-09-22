e32d8c111d7870049a088b0640fde018a925bf6f
Ali Akbar <2018-cs-2080@uosahiwal.edu.pk>
2022-09-21 23:46:36 -0700

    initial commit

--- UserManagement-main/src/main/java/com/usermanagement/repository/OrganizationRepository.java
+++ UserManagement-main/src/main/java/com/usermanagement/repository/OrganizationRepository.java
@@ -1,12 +1,9 @@
 package com.usermanagement.repository;
 
 import com.usermanagement.entity.Organization;
-import com.usermanagement.entity.User;
-import com.usermanagement.entity.UserOrganization;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.List;
 
 @Repository
